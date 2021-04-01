81b7cbfc0d44b4366a4aaac198c029934384418f
Mohammed Safwan <mohammed.saff333@gmail.com>
2021-04-01 15:59:33 +0530

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,32 @@
+t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+## Format date to Type Date
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+## Combine Date and Time column
+dateTime <- paste(t$Date, t$Time)
+
+## Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+## Remove Date and Time column
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+
+## Add DateTime column
+t <- cbind(dateTime, t)
+
+## Format dateTime Column
+t$dateTime <- as.POSIXct(dateTime)
+par(mar=c(4,4,4,4))
+## Create Plot 3
+with(t, {
+  plot(Sub_metering_1~dateTime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~dateTime,col='blue')
+  lines(Sub_metering_3~dateTime,col='red')
+})
+legend("topright", col=c("black", "blue", "red"), lwd=c(1,1,1), 
+       c("Sub_metering1", "Sub_metering2", "Sub_metering3"))
